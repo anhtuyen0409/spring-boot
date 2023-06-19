ced09b33ea8a892f369a01a08b8f50d8e8efea07
Tuyen <nguyenanhtuyen10a5@gmail.com>
2023-06-19 19:00:31 +0700

    first commit

---
+++ src/main/java/com/nguyenanhtuyen/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.nguyenanhtuyen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * class DemoApplication dùng để start ứng dụng 
+ */
+@SpringBootApplication
+public class DemoApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/nguyenanhtuyen/demo/MainController.java
@@ -0,0 +1,20 @@
+package com.nguyenanhtuyen.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String welcome() {
+		return "Welcome to my first spring boot demo.";
+	}
+	
+	@RequestMapping("/home")
+	public String index() {
+		return "index";
+	}
+}
